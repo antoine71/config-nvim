39f02770561ce160767f10f5452e5e9789b7e385
Antoine Rebillard <antoine.rebillard@pm.me>
2025-12-21 18:59:40 +0100

    add ftplugin for c

---
+++ ftplugin/c.lua
@@ -0,0 +1,6 @@
+-- ~/.config/nvim/ftplugin/c.lua
+
+vim.opt_local.expandtab = false -- Utilise de vraies tabulations (pas d'espaces)
+vim.opt_local.shiftwidth = 4 -- Taille de l'indentation à 4
+vim.opt_local.tabstop = 4 -- Largeur visuelle d'un caractère tab à 4
+vim.opt_local.softtabstop = 4 -- Nombre d'espaces insérés en mode insertion

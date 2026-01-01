9f0681aa22e6ef770abf13c6361066cf224c2214
Antoine Rebillard <antoine.rebillard@pm.me>
2026-01-01 18:16:55 +0100

    up

--- lua/options.lua
+++ lua/options.lua
@@ -69,3 +69,15 @@ vim.o.scrolloff = 10
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
 vim.o.confirm = true
+
+-- Activer le persistent undo
+vim.opt.undofile = true
+
+-- Définir un dossier spécifique pour ne pas polluer tes projets de fichiers ".un~"
+local undodir = vim.fn.stdpath 'data' .. '/undo'
+vim.opt.undodir = undodir
+
+-- Créer le dossier s'il n'existe pas
+if vim.fn.isdirectory(undodir) == 0 then
+  vim.fn.mkdir(undodir, 'p')
+end
